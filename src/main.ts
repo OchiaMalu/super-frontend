152ca94404a9ab8efcf2bf781740a6c503cea162
ochiamalu <linzhehao1108@126.com>
2023-05-08 22:45:00 +0800

    first init，前端框架初步搭建，搜索页面初步完成

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+import {Button, Icon, NavBar} from 'vant';
+import 'vant/lib/index.css';
+import * as VueRouter from 'vue-router'
+import routes from "./config/routes.ts";
+
+const router = VueRouter.createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: VueRouter.createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+})
+const app = createApp(App);
+app.use(Button);
+app.use(NavBar);
+app.use(Icon);
+app.use(router)
+app.mount('#app')
