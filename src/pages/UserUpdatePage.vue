563040bbae296144c4c44dc8410b037fb6eeaccc
ochiamalu <linzhehao1108@126.com>
2023-05-24 14:04:23 +0800

    feature: 用户头像上传input

--- src/pages/UserUpdatePage.vue
+++ src/pages/UserUpdatePage.vue
@@ -1,16 +1,17 @@
 <template>
     <template v-if="user">
-        <van-cell title="头像" is-link to="user/edit">
-            <img style="height: 48px" :src="user.avatarUrl">
+        <van-cell title="头像" is-link>
+            <van-uploader v-model="fileList" :max-count="1" preview-size="60">
+                <template #preview-delete/>
+            </van-uploader>
         </van-cell>
-        <van-cell title="昵称" to="user/edit" is-link :value="user.username"
-                  @click="toEdit('username','昵称',user.username)"/>
         <van-cell title="用户账号" :value="user.userAccount"/>
-        <van-cell title="性别" is-link to="user/edit" :value="user.gender"
+        <van-cell title="昵称" is-link :value="user.username"
+                  @click="toEdit('username','昵称',user.username)"/>
+        <van-cell title="性别" is-link :value="user.gender"
                   @click="toEdit('gender','性别',user.gender)"/>
-        <van-cell title="电话" is-link to="user/edit" :value="user.phone" @click="toEdit('phone','电话',user.phone)"/>
-        <van-cell title="邮箱" is-link to="user/edit" :value="user.email" @click="toEdit('email','邮箱',user.email)"/>
-        <!--        <van-cell title="注册时间" :value="user.createTime.toISOString()"/>-->
+        <van-cell title="电话" is-link :value="user.phone" @click="toEdit('phone','电话',user.phone)"/>
+        <van-cell title="邮箱" is-link :value="user.email" @click="toEdit('email','邮箱',user.email)"/>
     </template>
 </template>
 
@@ -18,17 +19,23 @@
 
 import {useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
-import myAxios from "../plugins/my-axios.js";
 import {showFailToast, showSuccessToast} from "vant";
 import {getCurrentUser} from "../services/user.ts";
 
 let router = useRouter();
+const fileList = ref([
+    {
+        url: '',
+        isImage: true
+    }
+]);
 const user = ref()
 onMounted(async () => {
     let currentUser = await getCurrentUser();
     if (currentUser) {
         showSuccessToast("获取个人信息成功")
         user.value = currentUser
+        fileList.value[0].url = currentUser.avatarUrl
     } else {
         showFailToast("未登录")
         router.replace("/user/login")
@@ -36,7 +43,7 @@ onMounted(async () => {
 })
 const toEdit = (editKey: string, editName: string, editValue: string) => {
     router.push({
-        path: 'user/edit',
+        path: '/user/edit',
         query: {
             editKey,
             editName,
@@ -47,5 +54,11 @@ const toEdit = (editKey: string, editName: string, editValue: string) => {
 </script>
 
 <style scoped>
+:deep(.van-uploader__upload) {
+    border-radius: 50%;
+}
 
+:deep(.van-uploader__preview-image) {
+    border-radius: 50%;
+}
 </style>
