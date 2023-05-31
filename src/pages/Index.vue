14fcf9abb5cf258133e09239077c2af23005eed6
ochiamalu <linzhehao1108@126.com>
2023-05-31 11:36:48 +0800

    fix: 修复首页刷新后下拉无法获取新列表

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,31 @@
 <template>
-    <div style="position: relative;height: 100%;width: 100%">
-<!--        <div style="margin-top: 50%" v-if="onLoading">-->
-<!--            <van-loading vertical >-->
-<!--                <template #icon>-->
-<!--                    <van-icon name="star-o" size="30"/>-->
-<!--                </template>-->
-<!--                加载中...-->
-<!--            </van-loading>-->
-<!--        </div>-->
-        <van-pull-refresh
-                v-model="refreshLoading"
-                success-text="刷新成功"
-                @refresh="onRefresh"
-        >
-            <van-list
-                    v-model:loading="listLoading"
-                    :finished="listFinished"
-                    offset="0"
-                    finished-text="没有更多了"
-                    @load="onLoad"
-            >
-                <UserCardList :user-list="userList"/>
-            </van-list>
-            <van-back-top right="20px" bottom="60px" />
-            <van-empty v-if="(!userList ||　userList.length===0) && !listLoading" image="search" description="暂无用户"/>
-        </van-pull-refresh>
-    </div>
+  <div style="position: relative;height: 100%;width: 100%">
+    <!--        <div style="margin-top: 50%" v-if="onLoading">-->
+    <!--            <van-loading vertical >-->
+    <!--                <template #icon>-->
+    <!--                    <van-icon name="star-o" size="30"/>-->
+    <!--                </template>-->
+    <!--                加载中...-->
+    <!--            </van-loading>-->
+    <!--        </div>-->
+    <van-pull-refresh
+        v-model="refreshLoading"
+        success-text="刷新成功"
+        @refresh="onRefresh"
+    >
+      <van-list
+          v-model:loading="listLoading"
+          :finished="listFinished"
+          offset="0"
+          finished-text="没有更多了"
+          @load="onLoad"
+      >
+        <UserCardList :user-list="userList"/>
+      </van-list>
+      <van-back-top right="20px" bottom="60px"/>
+      <van-empty v-if="(!userList ||　userList.length===0) && !listLoading" image="search" description="暂无用户"/>
+    </van-pull-refresh>
+  </div>
 
 
 </template>
@@ -42,30 +42,30 @@ const refreshLoading = ref(false)
 const currentPage = ref(0)
 
 async function getUserList(currentPage) {
-    const userListData = await myAxios.get("/user/match", {
-        params: {
-            currentPage: currentPage
-        }
+  const userListData = await myAxios.get("/user/match", {
+    params: {
+      currentPage: currentPage
+    }
+  })
+  if (userListData?.data.code === 0) {
+    showSuccessToast("加载成功")
+  } else {
+    showFailToast("加载失败")
+  }
+  if (userListData?.data.data.records.length === 0) {
+    listFinished.value = true
+    return
+  }
+  if (userListData?.data.data.records) {
+    userListData.data.data.records.forEach(user => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags)
+      }
     })
-    if (userListData?.data.code === 0) {
-        showSuccessToast("加载成功")
-    } else {
-        showFailToast("加载失败")
+    for (let i = 0; i < userListData.data.data.records.length; i++) {
+      userList.value.push(userListData.data.data.records[i])
     }
-    if (userListData?.data.data.records.length===0) {
-        listFinished.value = true
-        return
-    }
-    if (userListData?.data.data.records) {
-        userListData.data.data.records.forEach(user => {
-            if (user.tags) {
-                user.tags = JSON.parse(user.tags)
-            }
-        })
-        for (let i = 0; i < userListData.data.data.records.length; i++) {
-            userList.value.push(userListData.data.data.records[i])
-        }
-    }
+  }
 }
 
 // onMounted(async () => {
@@ -73,16 +73,17 @@ async function getUserList(currentPage) {
 //     onLoading.value = false
 // })
 const onLoad = async () => {
-    currentPage.value++
-    await getUserList(currentPage.value)
-    listLoading.value = false;
+  currentPage.value++
+  await getUserList(currentPage.value)
+  listLoading.value = false;
 }
 const onRefresh = async () => {
-    currentPage.value = 1
-    userList.value = []
-    await getUserList(currentPage.value)
-    refreshLoading.value = false
-    listLoading.value = false;
+  currentPage.value = 1
+  userList.value = []
+  listFinished.value = false
+  await getUserList(currentPage.value)
+  refreshLoading.value = false
+  listLoading.value = false;
 }
 
 
