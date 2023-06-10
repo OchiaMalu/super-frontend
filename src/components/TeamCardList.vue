42ce8245a5fefbf47da5f6508097fe25abc2abdc
ochiamalu <linzhehao1108@126.com>
2023-06-10 16:04:17 +0800

    fix: 修复队伍卡片click事件覆盖按钮事件

--- src/components/TeamCardList.vue
+++ src/components/TeamCardList.vue
@@ -2,16 +2,19 @@
     <van-card
         v-for="team in props.teamList"
         :desc="team.description"
-        :title="team.name"
         :thumb="defaultImg"
-        @click="getTeamDetail(team.id)"
+        @click-thumb="getTeamDetail(team.id)"
     >
+        <template #title>
+            <div class="team-title" @click="getTeamDetail(team.id)">{{ team.name }}</div>
+        </template>
         <template #tags>
-            <van-tag plain type="danger" style="margin-right: 8px;margin-top: 8px">{{ teamStatusEnum[team.status] }}
+            <van-tag plain type="danger" style="margin-right: 8px;margin-top: 8px" @click="getTeamDetail(team.id)">
+                {{ teamStatusEnum[team.status] }}
             </van-tag>
         </template>
         <template #bottom>
-            <div style="margin-top: 20px">
+            <div style="margin-top: 20px" @click="getTeamDetail(team.id)">
                 {{ '队伍人数：' + team.hasJoinNum + "/" + team.maxNum }}
             </div>
             <div v-if="team.expireTime">
@@ -108,7 +111,7 @@ const doQuitTeam = async (id: number) => {
         showSuccessToast("退出队伍成功")
         onRefresh()
     } else {
-        showFailToast("加入队伍失败" + (res.data.description ? `,${res.data.description}` : ''))
+        showFailToast("退出队伍失败" + (res.data.description ? `,${res.data.description}` : ''))
     }
 }
 
@@ -120,7 +123,7 @@ const doDeleteTeam = async (id: number) => {
         showSuccessToast("解散队伍成功")
         onRefresh()
     } else {
-        showFailToast("加入队伍失败" + (res.data.description ? `,${res.data.description}` : ''))
+        showFailToast("解散队伍失败" + (res.data.description ? `,${res.data.description}` : ''))
     }
 }
 
@@ -145,5 +148,23 @@ const getTeamDetail = (id) => {
 :deep(.van-image__img) {
     height: 128px;
     object-fit: unset;
+}
+
+.team-title {
+    color: rgb(50, 50, 51);
+    display: -webkit-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, "Segoe UI", Arial, Roboto, "PingFang SC", miui, "Hiragino Sans GB", "Microsoft Yahei", sans-serif;
+    font-size: 12px;
+    font-weight: 600;
+    height: 16px;
+    line-height: 16px;
+    max-height: 32px;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    text-overflow: ellipsis;
+    width: 247px;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 </style>
