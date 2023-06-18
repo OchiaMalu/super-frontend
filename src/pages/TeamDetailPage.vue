3b70e481a5d07595f0cede07fdfbdae4ef759f55
ochiamalu <linzhehao1108@126.com>
2023-06-18 09:32:55 +0800

    feature: 显示队伍成员

--- src/pages/TeamDetailPage.vue
+++ src/pages/TeamDetailPage.vue
@@ -1,8 +1,8 @@
 <template>
     <van-image
-            width="100%"
-            height="120"
-            :src="defaultImg"
+        width="100%"
+        height="120"
+        :src="defaultImg"
     />
     <van-cell-group inset>
         <van-cell title="队伍名" icon="flag-o" :value="team.name"/>
@@ -12,6 +12,12 @@
         <van-cell title="队伍人数" icon="friends-o" :value="`${team.hasJoinNum}/${team.maxNum}`"/>
         <van-cell title="过期时间" icon="clock-o" :value="team.expireTime || '永不过期'"/>
     </van-cell-group>
+    <van-divider
+        :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }"
+    >
+        队伍成员
+    </van-divider>
+    <user-card-list :user-list="teamMemberList"/>
 </template>
 
 <script setup lang="ts">
@@ -21,13 +27,24 @@ import {onMounted, ref} from "vue";
 import myAxios from "../plugins/my-axios.js";
 import {showFailToast} from "vant";
 import {teamStatusEnum} from "../constants/team.ts";
+import UserCardList from "../components/UserCardList.vue";
 
+const teamMemberList = ref()
 let route = useRoute();
 const team = ref({})
 onMounted(async () => {
     let res = await myAxios.get("/team/" + route.query.id);
     if (res?.data.code === 0) {
         team.value = res.data.data
+        let res_ = await myAxios.get("/team/member/" + route.query.id);
+        if (res_.data.code === 0) {
+            res_.data.data.forEach((user) => {
+                if (user.tags) {
+                    user.tags = JSON.parse(user.tags)
+                }
+            })
+            teamMemberList.value = res_.data.data
+        }
     } else {
         showFailToast("队伍查询失败")
     }
