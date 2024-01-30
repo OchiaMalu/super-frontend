12f538cb12bb92d4f1bf9bb2a31e6158ee872638
ochiamalu <linzhehao1108@126.com>
2024-01-30 11:11:11 +0800

    fix: 修复空标签时空指针

--- super-web/src/pages/SearchResultPage.vue
+++ super-web/src/pages/SearchResultPage.vue
@@ -1,24 +1,24 @@
 <template>
-    <div style="position: relative;height: 100%;width: 100%">
-        <van-pull-refresh
-                v-model="refreshLoading"
-                success-text="刷新成功"
-                @refresh="onRefresh"
-        >
-            <van-list
-                    v-model:loading="listLoading"
-                    :finished="listFinished"
-                    offset="0"
-                    finished-text="没有更多了"
-                    @load="onLoad"
-            >
-                <UserCardList :user-list="userList"/>
-            </van-list>
-        </van-pull-refresh>
-        <van-back-top right="20px" bottom="60px"/>
-        <van-empty v-if="(!userList ||　userList.length===0) && listLoading===false" image="search"
-                   description="暂无符合要求的用户"/>
-    </div>
+  <div style="position: relative;height: 100%;width: 100%">
+    <van-pull-refresh
+        v-model="refreshLoading"
+        success-text="刷新成功"
+        @refresh="onRefresh"
+    >
+      <van-list
+          v-model:loading="listLoading"
+          :finished="listFinished"
+          offset="0"
+          finished-text="没有更多了"
+          @load="onLoad"
+      >
+        <UserCardList :user-list="userList"/>
+      </van-list>
+    </van-pull-refresh>
+    <van-back-top right="20px" bottom="60px"/>
+    <van-empty v-if="(!userList ||　userList.length===0) && listLoading===false" image="search"
+               description="暂无符合要求的用户"/>
+  </div>
 </template>
 
 <script setup>
@@ -38,43 +38,43 @@ const listLoading = ref(false)
 const listFinished = ref(false)
 
 async function getSearchResult(currentPage) {
-    const res = await myAxios.get("/user/search/tags?currentPage=" + currentPage, {
-        params: {
-            tagNameList: tags
-        },
-        paramsSerializer: params => {
-            return qs.stringify(params, {indices: false})
-        }
-    })
-        .then(function (resp) {
-            return resp.data?.data
-        }).catch(function () {
-          showFailToast("搜索失败" + (res.data.description ? `,${res.data.description}` : ''))
-        })
-    if (res.records.length !== 0) {
-        res.records.forEach(user => {
-            if (user.tags) {
-                user.tags = JSON.parse(user.tags)
-            }
-        })
-        res.records.forEach((item) => userList.value.push(item))
-    } else {
-        listFinished.value = true
+  const res = await myAxios.get("/user/search/tags?currentPage=" + currentPage, {
+    params: {
+      tagNameList: tags
+    },
+    paramsSerializer: params => {
+      return qs.stringify(params, {indices: false})
     }
+  })
+  if (res.data.code === 0) {
+    if (res.data.data.records.length !== 0) {
+      res.data.data.records.forEach(user => {
+        if (user.tags) {
+          user.tags = JSON.parse(user.tags)
+        }
+      })
+      res.data.data.records.forEach((item) => userList.value.push(item))
+    } else {
+      listFinished.value = true
+    }
+  } else {
+    showFailToast("搜索失败" + (res.data.description ? `,${res.data.description}` : ''))
+    listFinished.value = true
+  }
 }
 
 const onLoad = async () => {
-    currentPage.value++
-    await getSearchResult(currentPage.value)
-    listLoading.value = false;
+  currentPage.value++
+  await getSearchResult(currentPage.value)
+  listLoading.value = false;
 }
 
 const onRefresh = async () => {
-    currentPage.value = 1
-    userList.value = []
-    await getSearchResult(currentPage.value)
-    refreshLoading.value = false
-    listLoading.value = false;
+  currentPage.value = 1
+  userList.value = []
+  await getSearchResult(currentPage.value)
+  refreshLoading.value = false
+  listLoading.value = false;
 }
 </script>
 
