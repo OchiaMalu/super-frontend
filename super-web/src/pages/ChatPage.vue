15309eadfa91eb4da6d6ebf80461ffa8ee616153
ochiamalu <linzhehao1108@126.com>
2025-02-28 12:44:10 +0800

    feat: 重构聊天页

--- super-web/src/pages/ChatPage.vue
+++ super-web/src/pages/ChatPage.vue
@@ -1,26 +1,57 @@
 <template>
-    <van-sticky>
-        <van-nav-bar
-            :title="title"
-            left-arrow
-            @click-left="onClickLeft"
-        >
-        </van-nav-bar>
-    </van-sticky>
-    <div class="chat-container">
-        <div class="content" ref="chatRoom" v-html="stats.content"></div>
-        <van-cell-group inset style="position: fixed;bottom: 0;width: 100%">
+    <div class="chat-page">
+        <!-- 顶部导航栏 -->
+        <div class="chat-header">
+            <van-nav-bar
+                :title="title"
+                left-arrow
+                @click-left="onClickLeft"
+                fixed
+            />
+        </div>
+
+        <!-- 聊天消息区域 -->
+        <div class="chat-messages" ref="chatRoom">
+            <div v-for="(message, index) in messages" :key="index" 
+                 :class="['message-item', message.isMy ? 'message-right' : 'message-left']">
+                <div class="message-avatar" @click="showUser(message.isMy ? message.fromUser.id : message.fromUser.id)">
+                    <img :src="message.isMy ? message.fromUser.avatarUrl : message.fromUser.avatarUrl" 
+                         :alt="message.isMy ? message.fromUser.username : message.fromUser.username">
+                </div>
+                <div class="message-content">
+                    <div class="message-name" v-if="!message.isMy">
+                        <span>{{ message.fromUser.username }}</span>
+                        <span class="message-time">{{ message.createTime }}</span>
+                    </div>
+                    <div class="message-bubble" 
+                         :class="{
+                             'admin-message': !message.isMy && message.isAdmin,
+                             'message-right-bubble': message.isMy
+                         }">
+                        {{ message.text }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- 底部输入框 -->
+        <div class="chat-input">
             <van-field
-                v-model="stats.text"
-                center
-                clearable
-                placeholder="聊点什么吧...."
+                v-model="inputText"
+                placeholder="发送消息..."
+                :border="false"
+                @keypress.enter="send"
             >
                 <template #button>
-                    <van-button size="small" type="primary" @click="send" style="margin-right: 16px">发送</van-button>
+                    <van-button 
+                        size="small" 
+                        type="primary" 
+                        @click="send"
+                        :disabled="!inputText.trim()"
+                    >发送</van-button>
                 </template>
             </van-field>
-        </van-cell-group>
+        </div>
     </div>
 </template>
 <script setup>
@@ -86,6 +117,8 @@ const stopHeartbeat = () => {
 const chatRoom = ref(null)
 const DEFAULT_TITLE = "聊天"
 const title = ref(DEFAULT_TITLE)
+const messages = ref([])
+
 onMounted(async () => {
     let {id, username, userType, teamId, teamName, teamType} = route.query
     stats.value.chatUser.id = Number.parseInt(id)
@@ -104,48 +137,55 @@ onMounted(async () => {
     }
     stats.value.user = await getCurrentUser()
 
-
     // 私聊
     if (stats.value.chatType === stats.value.chatEnum.PRIVATE_CHAT) {
-        const privateMessage = await myAxios.post("/chat/privateChat",
-            {
-                toId: stats.value.chatUser.id,
-            })
+        const privateMessage = await myAxios.post("/chat/privateChat", {
+            toId: stats.value.chatUser.id,
+        })
         privateMessage.data.data.forEach(chat => {
-            if (chat.isMy === true) {
-                createContent(null, chat.fromUser, chat.text)
-            } else {
-                createContent(chat.toUser, null, chat.text, null, chat.createTime)
-            }
+            messages.value.push({
+                isMy: chat.isMy,
+                fromUser: chat.isMy ? chat.fromUser : chat.toUser,
+                text: chat.text,
+                createTime: chat.createTime
+            })
         })
     }
+    
+    // 大厅聊天
     if (stats.value.chatType === stats.value.chatEnum.HALL_CHAT) {
         const hallMessage = await myAxios.get("/chat/hallChat")
         hallMessage.data.data.forEach(chat => {
-            if (chat.isMy === true) {
-                createContent(null, chat.fromUser, chat.text)
-            } else {
-                createContent(chat.fromUser, null, chat.text, chat.isAdmin, chat.createTime)
-            }
-        })
-    }
-    if (stats.value.chatType === stats.value.chatEnum.TEAM_CHAT) {
-        const teamMessage = await myAxios.post("/chat/teamChat",
-            {
-                teamId: stats.value.team.teamId
+            messages.value.push({
+                isMy: chat.isMy,
+                fromUser: chat.fromUser,
+                text: chat.text,
+                isAdmin: chat.isAdmin,
+                createTime: chat.createTime
             })
-        teamMessage.data.data.forEach(chat => {
-            if (chat.isMy === true) {
-                createContent(null, chat.fromUser, chat.text)
-            } else {
-                createContent(chat.fromUser, null, chat.text, chat.isAdmin, chat.createTime)
-            }
         })
     }
+    
+    // 队伍聊天
+    if (stats.value.chatType === stats.value.chatEnum.TEAM_CHAT) {
+        const teamMessage = await myAxios.post("/chat/teamChat", {
+            teamId: stats.value.team.teamId
+        })
+        teamMessage.data.data.forEach(chat => {
+            messages.value.push({
+                isMy: chat.isMy,
+                fromUser: chat.fromUser,
+                text: chat.text,
+                isAdmin: chat.isAdmin,
+                createTime: chat.createTime
+            })
+        })
+    }
+    
     init()
     await nextTick()
-    const lastElement = chatRoom.value.lastElementChild
-    lastElement.scrollIntoView()
+    const chatRoom = document.querySelector('.chat-messages')
+    chatRoom.scrollTop = chatRoom.scrollHeight
 })
 
 const init = () => {
@@ -180,7 +220,6 @@ const init = () => {
                 stats.value.users = data.users.filter(user => {
                     return user.id !== uid
                 })
-                // 获取当前连接的所有用户信息，并且排除自身，自己不会出现在自己的聊天列表里
             } else {
                 let flag;
                 if (stats.value.chatType === data.chatType) {
@@ -196,15 +235,18 @@ const init = () => {
                     flag = (data.fromUser?.id != uid)
                 }
                 if (flag) {
-                    stats.value.messages.push(data)
-                    // 构建消息内容
-                    createContent(data.fromUser, null, data.text, data.isAdmin, data.createTime)
+                    messages.value.push({
+                        isMy: false,
+                        fromUser: data.fromUser,
+                        text: data.text,
+                        isAdmin: data.isAdmin,
+                        createTime: data.createTime
+                    })
                 }
                 nextTick(() => {
-                    const lastElement = chatRoom.value.lastElementChild
-                    lastElement.scrollIntoView()
+                    const chatRoom = document.querySelector('.chat-messages')
+                    chatRoom.scrollTop = chatRoom.scrollHeight
                 })
-                flag = null;
             }
         };
         //关闭事件
@@ -227,29 +269,40 @@ const send = () => {
         showFailToast("不能给自己发信息")
         return;
     }
-    if (!stats.value.text.trim()) {
+    if (!inputText.value.trim()) {
         showFailToast("请输入内容")
-    } else {
-        if (typeof (WebSocket) == "undefined") {
-            showFailToast("您的浏览器不支持WebSocket")
-        } else {
-            let message = {
-                fromId: stats.value.user.id,
-                toId: stats.value.chatUser.id,
-                text: stats.value.text,
-                chatType: stats.value.chatType,
-                teamId: stats.value.team.teamId,
-            }
-            socket.send(JSON.stringify(message));
-            stats.value.messages.push({user: stats.value.user.id, text: stats.value.text})
-            createContent(null, stats.value.user, stats.value.text)
-            stats.value.text = '';
-            nextTick(() => {
-                const lastElement = chatRoom.value.lastElementChild
-                lastElement.scrollIntoView()
-            })
-        }
+        return;
     }
+    
+    if (typeof (WebSocket) == "undefined") {
+        showFailToast("您的浏览器不支持WebSocket")
+        return;
+    }
+    
+    let message = {
+        fromId: stats.value.user.id,
+        toId: stats.value.chatUser.id,
+        text: inputText.value,
+        chatType: stats.value.chatType,
+        teamId: stats.value.team.teamId,
+    }
+    
+    socket.send(JSON.stringify(message));
+    
+    // 添加自己的消息到显示列表
+    messages.value.push({
+        isMy: true,
+        fromUser: stats.value.user,
+        text: inputText.value,
+        createTime: new Date().toLocaleTimeString()
+    })
+    
+    inputText.value = '';
+    
+    nextTick(() => {
+        const chatRoom = document.querySelector('.chat-messages')
+        chatRoom.scrollTop = chatRoom.scrollHeight
+    })
 }
 
 const showUser = (id) => {
@@ -261,109 +314,118 @@ const showUser = (id) => {
     })
 }
 
-/**
- * 这个方法是用来将 json的聊天消息数据转换成 html的。
- */
-const createContent = (remoteUser, nowUser, text, isAdmin, createTime) => {
-    // 当前用户消息
-    let html;
-    if (nowUser) {
-        // nowUser 表示是否显示当前用户发送的聊天消息，绿色气泡
-        html = `
-    <div class="message self">
-    <div class="myInfo info">
-      <img :alt=${nowUser.username} class="avatar" onclick="showUser(${nowUser.id})" src=${nowUser.avatarUrl}>
-    </div>
-      <p class="text">${text}</p>
-    </div>
-`
-    } else if (remoteUser) {
-        // remoteUser表示远程用户聊天消息，灰色的气泡
-        html = `
-     <div class="message other">
-      <img :alt=${remoteUser.username} class="avatar" onclick="showUser(${remoteUser.id})" src=${remoteUser.avatarUrl}>
-    <div class="info">
-      <span class="username">${remoteUser.username.length < 10 ? remoteUser.username : remoteUser.username}&nbsp;&nbsp;&nbsp;${createTime}</span>
-      <p class="${isAdmin ? 'admin text' : 'text'}" >${text}</p>
-    </div>
-    </div>
-`
-    }
-    stats.value.content += html;
-}
+const inputText = ref('')
+
 window.showUser = showUser
 </script>
 <style>
-.chat-container {
+.chat-page {
+    height: 100vh;
     display: flex;
     flex-direction: column;
+    background-color: #f7f7f7;
+}
+
+.chat-header {
+    height: 46px;
+}
+
+.chat-messages {
+    flex: 1;
+    overflow-y: auto;
+    padding: 60px 12px 56px;
+}
+
+.message-item {
+    display: flex;
+    margin-bottom: 16px;
+    gap: 8px;
+}
+
+.message-right {
+    flex-direction: row-reverse;
+}
+
+.message-avatar {
+    width: 40px;
+    height: 40px;
+    flex-shrink: 0;
+}
+
+.message-avatar img {
     width: 100%;
     height: 100%;
-    overflow-y: auto;
-}
-
-.message {
-    display: flex;
-    align-items: center;
-    margin: 10px 10px;
-}
-
-.content {
-    padding-top: 22px;
-    padding-bottom: 57px;
-    display: flex;
-    flex-direction: column
-}
-
-.self {
-    align-self: flex-end;
-}
-
-.avatar {
-    align-self: flex-start;
-    width: 35px;
-    height: 35px;
     border-radius: 50%;
-    margin-right: 10px;
-    margin-left: 10px;
+    object-fit: cover;
 }
 
-.username {
-    align-self: flex-start;
-    text-align: center;
-    max-width: 200px;
+.message-content {
+    max-width: 70%;
+    min-width: 0;
+}
+
+.message-name {
     font-size: 12px;
     color: #999;
-    padding-bottom: 4px;
-    white-space: nowrap;
-    overflow: visible;
-    background-color: #fff;
-}
-
-.info {
+    margin-bottom: 4px;
     display: flex;
-    flex-direction: column;
-    order: 2;
+    align-items: center;
+    max-width: 100%;
 }
 
-.myInfo {
-    align-self: flex-start;
+.message-name span:first-child {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    flex: 1;
 }
 
-.text {
-    padding: 10px;
-    border-radius: 10px;
-    background-color: #eee;
-    word-wrap: break-word;
-    word-break: break-all;
+.message-time {
+    margin-left: 8px;
+    font-size: 11px;
+    flex-shrink: 0;
 }
 
-.other .text {
-    align-self: flex-start;
+.message-bubble {
+    padding: 10px 16px;
+    border-radius: 16px;
+    background-color: #fff;
+    font-size: 14px;
+    line-height: 1.4;
+    word-break: break-word;
+    max-width: 100%;
+    display: inline-block;
 }
 
-.self .text {
-    background-color: #0084ff;
+.message-right-bubble {
+    background-color: #007AFF;
     color: #fff;
+}
+
+.admin-message {
+    background-color: #FFE7BA !important;
+    color: #B45F06 !important;
+}
+
+.chat-input {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #fff;
+    padding: 4px 12px;
+    border-top: 1px solid #eee;
+}
+
+:deep(.van-field__body) {
+    background-color: #f5f5f5;
+    border-radius: 20px;
+    padding: 2px 12px;
+    min-height: 36px;
+}
+
+:deep(.van-button--small) {
+    height: 32px;
+    line-height: 30px;
 }
 </style>
