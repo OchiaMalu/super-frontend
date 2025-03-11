7ac75c55d6ab15fd6b7838be9fb0772eefe5185e
ochiamalu <linzhehao1108@126.com>
2025-03-11 11:29:21 +0800

    build(vite): 移除 base 配置项

--- super-web/vite.config.ts
+++ super-web/vite.config.ts
@@ -3,7 +3,6 @@ import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 
 export default {
-    base: "./",
     plugins: [
         vue(),
         Components({
